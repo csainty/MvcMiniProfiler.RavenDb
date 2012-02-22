26b331b587287d013c9f8cc3f70ae44167170703
Chris Sainty <chris.sainty@quids.com.au>
2012-02-22 11:50:12 +1100

    Code reformat. Nothing to see here.

--- MvcMiniProfiler.RavenDb/Profiler.cs
+++ MvcMiniProfiler.RavenDb/Profiler.cs
@@ -7,28 +7,32 @@ using Raven.Client.Document;
 
 namespace MvcMiniProfiler.RavenDb
 {
-	public class Profiler
-	{
-		private static ConcurrentDictionary<string, IDisposable> _Requests = new ConcurrentDictionary<string, IDisposable>();
+    public class Profiler
+    {
+        private static ConcurrentDictionary<string, IDisposable> _Requests = new ConcurrentDictionary<string, IDisposable>();
 
-		public static void AttachTo(DocumentStore store) {
-			store.SessionCreatedInternal += TrackSession;
-			store.JsonRequestFactory.ConfigureRequest += BeginRequest;
-			store.JsonRequestFactory.LogRequest += EndRequest;
-		}
+        public static void AttachTo(DocumentStore store)
+        {
+            store.SessionCreatedInternal += TrackSession;
+            store.JsonRequestFactory.ConfigureRequest += BeginRequest;
+            store.JsonRequestFactory.LogRequest += EndRequest;
+        }
 
-		private static void TrackSession(InMemoryDocumentSessionOperations obj) {
-			MvcMiniProfiler.MiniProfiler.Current.Step("RavenDb: Created Session").Dispose();
-		}
+        private static void TrackSession(InMemoryDocumentSessionOperations obj)
+        {
+            MvcMiniProfiler.MiniProfiler.Current.Step("RavenDb: Created Session").Dispose();
+        }
 
-		private static void BeginRequest(object sender, WebRequestEventArgs e) {
-			_Requests.TryAdd(e.Request.RequestUri.PathAndQuery, MvcMiniProfiler.MiniProfiler.Current.Step("RavenDb: Query - " + e.Request.RequestUri.PathAndQuery));
-		}
+        private static void BeginRequest(object sender, WebRequestEventArgs e)
+        {
+            _Requests.TryAdd(e.Request.RequestUri.PathAndQuery, MvcMiniProfiler.MiniProfiler.Current.Step("RavenDb: Query - " + e.Request.RequestUri.PathAndQuery));
+        }
 
-		private static void EndRequest(object sender, RequestResultArgs e) {
-			IDisposable request;
-			if (_Requests.TryRemove(e.Url, out request))
-				request.Dispose();
-		}
-	}
+        private static void EndRequest(object sender, RequestResultArgs e)
+        {
+            IDisposable request;
+            if (_Requests.TryRemove(e.Url, out request))
+                request.Dispose();
+        }
+    }
 }
